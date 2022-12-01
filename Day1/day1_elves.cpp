869fd7a976412799192fc462478b789646b87fd2
Adin Lukas <Lukeisun57@gmail.com>
2022-12-01 03:14:20 -0500

    First day of advent of code, C++ is great

---
+++ Day1/day1_elves.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <queue>
+
+int main() {
+  std::ifstream file ("input.in");
+  string line;
+  if (file.is_open()) {
+    int currCount = 0;
+    priority_queue<int> top;
+    while (std::getline(file, line)) {
+        if (line.empty()) {
+          top.push(currCount);
+          currCount = 0;
+          continue;
+        }
+        currCount += std::stoi(line);
+      }
+      // std::cout << line << '\n';
+      int sum = 0;
+      for (int i = 0; i < 3; i++) {
+        std::cout << top.top() << std::endl;
+        sum += top.top();
+        top.pop();
+      }
+      std::cout <<  sum << '\n';
+    }
+    return 0;
+}
+
+
+
