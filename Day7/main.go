1f2dcfb1e81fc1d04ccb0f8901fcd279f25de5a2
Adin Lukas <Lukeisun57@gmail.com>
2022-12-07 19:32:13 -0500

    wow i finally got it

--- Day7/main.go
+++ Day7/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"math"
 	"os"
 	"strconv"
 	"strings"
@@ -20,21 +21,27 @@ func main() {
 	scanner.Scan()
 	root := Dir{"/", 0, nil, []*Dir{}}
 	currDir := &root
-	directoryList := make(map[string]*Dir)
-	directoryList["/"] = &root
 	for scanner.Scan() {
 		s := scanner.Text()
 		if strings.Contains(s, "cd") {
-			currDir = cd(currDir, s, directoryList)
+			currDir = cd(currDir, s)
 		} else if strings.Contains(s, "ls") {
-			currDir = ls(scanner, currDir, directoryList)
+			currDir = ls(scanner, currDir)
 		}
 	}
-	sum := 0 
+	sum := 0
+	// Part 1
 	findAllLessThan100k(&root, &sum)
 	fmt.Println(sum)
+	// Part 2
+	available := 70000000 - root.size
+	needed := 30000000
+	max := math.MaxInt
+	smallest := 0
+	smallestToDelete(&root, available, &max, needed, &smallest)
+	fmt.Println(smallest)
 }
-func findAllLessThan100k(currDir *Dir, sum *int){
+func findAllLessThan100k(currDir *Dir, sum *int) {
 	if currDir.size < 100000 {
 		*sum += currDir.size
 	}
@@ -42,51 +49,51 @@ func findAllLessThan100k(currDir *Dir, sum *int){
 		findAllLessThan100k(currDir.children[i], sum)
 	}
 }
-// function that change reference of passed in struct
-func cd(currDir *Dir, s string, directoryList map[string]*Dir) *Dir {
+func smallestToDelete(currDir *Dir, available int, max *int, needed int, smallest *int) {
+	if currDir.size > needed-available && currDir.size+available < *max {
+		*max = currDir.size + available
+		*smallest = currDir.size
+	}
+	for i := 0; i < len(currDir.children); i++ {
+		smallestToDelete(currDir.children[i], available, max, needed, smallest)
+	}
+}
+
+func cd(currDir *Dir, s string) *Dir {
 	strs := strings.Split(s, " ")
 	if strs[2] == ".." {
 		return currDir.parent
-	} else if val, ok := (directoryList)[strs[2]]; ok {
-		// fmt.Println("hi")
-		// fmt.Println(strs[2])
-		// fmt.Println(currDir)
-		// fmt.Printf("%p\n", currDir)
-		// fmt.Printf("%p\n", val)
-		return val
+	} else {
+		for i := 0; i < len(currDir.children); i++ {
+			if currDir.children[i].name == strs[2] {
+				return currDir.children[i]
+			}
+		}
 	}
-	fmt.Println("err")
-	return currDir
+	return nil
 }
 
-func ls(scanner *bufio.Scanner, currDir *Dir, directoryList map[string]*Dir) *Dir {
+func ls(scanner *bufio.Scanner, currDir *Dir) *Dir {
 	for scanner.Scan() {
 		s := scanner.Text()
 		strs := strings.Split(s, " ")
-		// fmt.Printf("%p\n", currDir)
 		if strs[1] == "cd" {
-			currDir = cd(currDir, s, directoryList)
+			currDir = cd(currDir, s)
 			return currDir
 		}
 		if strs[0] == "dir" {
-			if directoryList[strs[1]] == nil {
-				temp := Dir{strs[1], 0, currDir, []*Dir{}}
-				currDir.children = append(currDir.children, &temp)
-				directoryList[strs[1]] = &temp
-			}
+			name := strs[1]
+			temp := Dir{name, 0, currDir, []*Dir{}}
+			currDir.children = append(currDir.children, &temp)
 			continue
 		}
 		amount, _ := strconv.Atoi(strs[0])
 		currDir.size += amount
 		curr := currDir.parent
-		// fmt.Println("hi", currDir)
-		// fmt.Printf("%p\n", currDir)
-		// fmt.Println(currDir.parent)
-		// fmt.Println(curr)
 		for curr != nil {
 			curr.size += amount
 			curr = curr.parent
 		}
 	}
-	return currDir
+	return nil
 }
